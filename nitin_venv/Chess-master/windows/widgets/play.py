276a7ad5fc354ad207b5dc7ddd151fce24d5643e
mtn.19102001 <mtn.19102001@gmail.com>
2022-08-05 08:49:27 +0700

    update

--- nitin_venv/Chess-master/windows/widgets/play.py
+++ nitin_venv/Chess-master/windows/widgets/play.py
@@ -76,7 +76,7 @@ class Play(QWidget):
                     for x in responseObject:
                         self.socketSignal.emit(x)
             self.clearDisplay()
-            time.sleep(20)
+            time.sleep(10)
             currentWidgetIndex = self.mainwindow.getCurrentIndex()
             # print(currentWidgetIndex)
             if (playWidgetIndex != currentWidgetIndex):
